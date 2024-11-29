352231cb5a8fad506d1196aa2f7a08db65df1843
YurDan1973 <jura.danilov.73@gmail.com>
2024-11-29 14:22:47 +0300

    Initial commit

--- src/main/java/com/hstn/crud_empl/rest/EmployeeRestController.java
+++ src/main/java/com/hstn/crud_empl/rest/EmployeeRestController.java
@@ -1,7 +1,7 @@
 package com.hstn.crud_empl.rest;
 
-import com.hstn.crud_empl.dao.EmployeeDAO;
 import com.hstn.crud_empl.entity.Employee;
+import com.hstn.crud_empl.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,17 +13,18 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private EmployeeDAO employeeDAO;
+//    private EmployeeDAO employeeDAO;
+private EmployeeService employeeService;
 
     @Autowired
     // В этом случае аннотация эта не обязательна, т.к. в
     // этом классе только один конструктор
-    public EmployeeRestController(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeService.findAll();
     }
 }
